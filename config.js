cbf43d6b31345552693e2411df68b3df1a48cc50
Scarour <maleknabil2010@gmail.com>
2025-07-19 08:51:14 +0300

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ module.exports = {
         ownerID: '721063236371480717', // ← Insert the Server Owner ID.
         supportId: '1395996465364733983', // ← Insert General Support Category ID.
         premiumId: '1395996499812290671', // ← Insert Premium Support Category ID.
-        whitelist: '1389475186708971566', // ← Insert Whitelist Role ID (People with this role are allowed to manage tickets).
+        whitelist: '1395997857471004805', // ← Insert Whitelist Role ID (People with this role are allowed to manage tickets).
         staff: '1395997857471004805', // ← Insert Staff Role to be pinged upon ticket creation.
     },
     logs: {
